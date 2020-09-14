e355d2e7a3a3a7056859d59e462861a0d698c336
Julien Giraud <giraud740@gmail.com>
2020-09-14 22:45:25 +0200

    Add ideas in test file

--- src/test/java/fr/univ_lyon1/info/m1/stopcovid_simulator/model/StopCovidUserModelTest.java
+++ src/test/java/fr/univ_lyon1/info/m1/stopcovid_simulator/model/StopCovidUserModelTest.java
@@ -30,4 +30,23 @@ class UserModelTest {
         assertThat(i, is(42));
     }
 
+    @Test
+    void differentIdCheck() {
+        // Given
+        User a = new User("");
+
+        // When
+        User b = new User("");
+
+        assertThat(b.getId(), is(a.getId() + 1));
+    }
+
+    // TODO test :
+    //  Vérifier qu'un utilisateur déclaré INFECTED passe bien à INFECTED
+    //  Vérifier qu'un utilisateur NO_RISKY passe en RISKY après 2 contacts déjà INFECTED
+    //  Vérifier qu'un utilisateur NO_RISKY passe en RISKY après 2 contacts qui passent en INFECTED
+    //  Vérifier qu'un utilisateur INFECTED ne peut pas passer en RISKY
+    //  Vérifier que le nombre de contacts INFECTED se met bien à jour
+    //  Vérifier que a.meet(b) est équivalent à b.meet(a)
+
 }
